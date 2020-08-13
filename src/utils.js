69c5f78036d02a3df41e1c68c3f5c5d050d80b66
davidgortega <g.ortega.david@gmail.com>
2020-08-13 17:45:42 +0200

    ubuntu

--- src/utils.js
+++ src/utils.js
@@ -17,27 +17,30 @@ const exec = async (command, opts) => {
 
 const setup_dvc = async opts => {
   const { version } = opts;
+  const { platform } = process;
+
+  console.log(platform);
 
   let sudo = '';
   try {
     sudo = await exec('which sudo');
   } catch (err) {}
 
-  if (process.platform === 'linux');
+  if (platform === 'linux');
   console.log(
     await exec(`${sudo} wget https://dvc.org/deb/dvc.list -O /etc/apt/sources.list.d/dvc.list && \
       ${sudo} apt update && \
       ${sudo} apt -y install dvc${version !== 'latest' ? `=${version}` : ''}`)
   );
 
-  if (process.platform === 'darwin');
+  if (platform === 'darwin');
   console.log(
     await exec(
       `${sudo} brew install dvc${version !== 'latest' ? `@${version}` : ''}`
     )
   );
 
-  if (process.platform === 'win32');
+  if (platform === 'win32');
   console.log(
     await exec(
       `choco install dvc${version !== 'latest' ? ` --version ${version}` : ''}`
